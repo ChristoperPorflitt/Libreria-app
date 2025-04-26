2d338b2377f23a956129a5c94679b2f4ecaff1c1
Chris-porflitt <Alexander.porflitt1912@gmail.com>
2025-04-25 21:40:39 -0400

    Prueba visual

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Prueba de titulo</title>
+        <title>Prueba de tituloooooo</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
